da233be79c36a07a0d7f6127f6a4ab0a5fabf144
shiyu19941811 <1398920917@qq.com>
2017-06-13 06:02:21 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
-      resources :resumes 
+      resources :resumes
     end
   end
 
   resources :jobs do
     resources :resumes
   end
-  root 'jobs#index'
+  root 'welcome#index'
 end
